eac18361e5909910852b10b84e98f9c0a9354e18
nicsylvia <nicsylvia15f@gmail.com>
2023-04-29 16:43:16 -0700

    saved shecodes training

---
+++ SheCodesTechSisMeetUp.ts
@@ -0,0 +1,48 @@
+// const SheCodesArray = ["Faith", "Blessing", "Destiny", "5 girls"];
+
+// console.log("Array of strings: ", SheCodesArray);
+
+const SheCodesNumber = [1, 2, 3, 5, 7];
+// console.log("Array of number: ", SheCodesNumber);
+
+// const ArrayInArray = [
+//   1,
+//   3,
+//   "Precious",
+//   [1, 3],
+//   "SheCodes",
+//   { name: "Chinaza", age: 19 },
+// ];
+// console.log("Array of different types: ", ArrayInArray);
+
+// SIMPLE ARRAY METHODS:
+// 1. POP(Remove from an array)
+// const SheCodes = ["Destiny", "Divine", "Precious", "Dorcas"];
+// const RemoveLastPerson = SheCodes.pop();
+// // console.log("The she codes array: ", SheCodes);
+// // console.log("Removing the last person which is: ", RemoveLastPerson);
+
+// // PUSH
+// const AddToSheCodes = SheCodes.push("Jemima");
+// console.log("Adding to she codes: ", SheCodes);
+
+// // UNSHIFT:
+// const AddAgain = SheCodes.unshift("Favour");
+// console.log("Adding again to shecodes array: ", SheCodes);
+
+// // SHIFT:
+// const RemoveFromFront = SheCodes.shift();
+// console.log(RemoveFromFront);
+// console.log(SheCodes);
+
+const sylviaShoppingCart = ["Milk", "Coffee", "Tea", "Honey"];
+
+console.log(sylviaShoppingCart.unshift("meat"));
+
+console.log(sylviaShoppingCart.pop());
+
+console.log(sylviaShoppingCart.push("Bread"));
+
+console.log(sylviaShoppingCart.shift());
+
+console.log("this is sylviaShoppingCart", sylviaShoppingCart);
