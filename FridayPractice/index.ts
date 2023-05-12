64bcf6218c504e2a95c08d7e96dc9d3db7877c29
nicsylvia <nicsylvia15f@gmail.com>
2023-05-12 05:15:25 -0700

    compiled question

---
+++ FridayPractice/index.ts
@@ -0,0 +1,30 @@
+// 1.Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+// Example :
+
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) // => returns "(123) 456-7890"
+console.log([2, 4, 5, 6, 7, 8, 5, 9].join().slice(0, 5));
+// const CreatePhoneNumber = (arr: any) => {
+//   return;
+// };
+
+// 2.You are given an array (which will have a length of at least 3, but could be very large) containing integers.
+// The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N.
+// Write a method that takes the array as an argument and returns N.
+
+// For example:
+
+// [2, 4, 0, 100, 4, 11, 2602, 36]
+
+// Should return: 11
+
+//   return evenNum[0];
+// }
+
+// 3.Your job is to write a function which increments a string, to create a new string. If the string already ends with a number, the number should be incremented by 1. If the string does not end with a number the number 1 should be appended to the new string.
+
+// Examples:
+
+// foo -> foo1
+
+// foobar23 -> foobar24
